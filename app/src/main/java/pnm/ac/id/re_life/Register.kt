b19d70e6c92b1459abd5f891c71218bd35f8b1a2
Zihan30 <cahyaam.zi25@gmail.com>
2024-11-20 16:12:21 +0700

    Next

--- app/src/main/java/pnm/ac/id/re_life/Register.kt
+++ app/src/main/java/pnm/ac/id/re_life/Register.kt
@@ -1,30 +1,51 @@
 package pnm.ac.id.re_life
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import de.hdodenhof.circleimageview.CircleImageView
+import java.util.*
 
-class Register : ComponentActivity(), View.OnClickListener{
+class Register : ComponentActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
-        val button2: Button = findViewById(R.id.button2)
-        val button3: Button = findViewById(R.id.button3)
+        val RegisCustomer: Button = findViewById(R.id.btnRegCus)
+        val RegisService: Button = findViewById(R.id.btnRegSer)
+        val btnLoginCustomer: Button = findViewById(R.id.btnLoginCustomer)
+        val btnLoginService: Button = findViewById(R.id.btnLoginService)
 
-        button2.setOnClickListener(this)
-        button3.setOnClickListener(this)
+        RegisCustomer.setOnClickListener(this)
+        RegisService.setOnClickListener(this)
+        btnLoginCustomer.setOnClickListener(this)
+        btnLoginService.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.button2 -> {
+            R.id.btnRegCus -> {
                 val pindahIntent = Intent(this, RegisterCustomer::class.java)
                 startActivity(pindahIntent)
             }
-            R.id.button3 -> {
+            R.id.btnRegSer -> {
                 val pindahIntent = Intent(this, RegisterService::class.java)
+                startActivity(pindahIntent)
+            }
+            R.id.btnLoginCustomer -> {
+                val pindahIntent = Intent(this, LoginCustomer::class.java)
+                startActivity(pindahIntent)
+            }
+            R.id.btnLoginService -> {
+                val pindahIntent = Intent(this, LoginService::class.java)
                 startActivity(pindahIntent)
             }
         }
