69ed611a2775f704129a8aa84726e9a1ad1c2c05
Zihan30 <cahyaam.zi25@gmail.com>
2024-12-03 16:17:54 +0700

    next

--- app/src/main/java/pnm/ac/id/re_life/ActivityService.kt
+++ app/src/main/java/pnm/ac/id/re_life/ActivityService.kt
@@ -7,11 +7,11 @@ import androidx.core.view.GravityCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 //import kotlinx.android.synthetic.main.activity_home_service.*
 
-class HomeService : AppCompatActivity() {
+class ActivityService : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_service)
+        setContentView(R.layout.activity_service)
 
         // Handle BottomNavigation
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
@@ -31,23 +31,5 @@ class HomeService : AppCompatActivity() {
                 else -> false
             }
         }
-
-//        // Handle Profile Icon Click
-//        profile_image.setOnClickListener {
-//            startActivity(Intent(this, ProfileService::class.java))
-//        }
-//
-//        // Handle "Tata Cara" ImageView Click
-//        findViewById<ImageView>(R.id.ic_tatacara).setOnClickListener {
-//            startActivity(Intent(this, PengelolaanService::class.java))
-//        }
-//
-//        // Placeholder: Set user data dynamically
-//        val userName = "Zihan Cahya Amelia"
-//        val userAddress = "Jl. Raya Ponorogo No. 123, Madiun"
-//        findViewById<TextView>(R.id.halo_text).text = "Halo, $userName"
-//        findViewById<TextView>(R.id.subtitle_text).text = userAddress
-//
-//        // Placeholder: Implement dynamic map details if needed
     }
 }
